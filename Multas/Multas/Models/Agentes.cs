fc81f0cf3b697c79703da00533b0d50780fdadc3
Joao Silva <aluno20451@ipt.pt>
2019-04-04 15:28:40 +0100

    Atualização dos atributos de todas as classes  do Modelo

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -9,10 +9,15 @@ namespace Multas.Models
     {
         // id, nome, esquadra, foto
 
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Esquadra { get; set; }
-        public int Foto { get; set; }
+        public int ID { get; set; }
 
+        public string Nome { get; set; }
+
+        public string Esquadra { get; set; }
+
+        public string Fotografia { get; set; }
+
+        //identifica as multas passadasa pelo Agente
+        public ICollection<Multas> ListaMultas { get; set; }
     }
 }
--- Multas/Multas/Models/Multas.cs
+++ Multas/Multas/Models/Multas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +9,33 @@ namespace Multas.Models
     public class Multas
     {
         // id, data, valor, infracao, FK viatura, FK agente, FK condutor
-        public int Id { get; set; }
 
-        public DateTime Data { get; set; }
-
-        public decimal Valor { get; set; }
+        public int ID { get; set; }
 
         public string Infracao { get; set; }
 
+        public string LocalDaMulta { get; set; }
 
+        public decimal ValorMulta { get; set; }
+
+        public DateTime DataDaMulta { get; set; }
+
+        //CRIAR CHAVES FORASTEIRAS
+
+        //FK para os Agentes
+        [ForeignKey("Agente")]
+        public int AgenteFK { get; set; }
+        public Agentes Agente { get; set; }
+        
+        //FK para os Condutores
+        [ForeignKey("Condutor")]
+        public int CondutorFK { get; set; }
+        public Condutores Condutor { get; set; }
+
+        //FK para as viaturas
+        [ForeignKey("Viatura")]
+        public int ViaturaFK { get; set; }
+        public Viaturas Viatura { get; set; }
 
     }
 }
